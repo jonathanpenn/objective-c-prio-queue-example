ea843cd3bd52a2d4c700c774f49d590cce5fe36f
Joshua Smith <kognate@gmail.com>
2014-01-20 21:38:15 -0500

    Initial Commit

---
+++ PrioQueueProject/PrioQueueProject.h
@@ -0,0 +1,13 @@
+//
+//  PrioQueueProject.h
+//  PrioQueueProject
+//
+//  Created by Josh Smith on 1/20/14.
+//  Copyright (c) 2014 Josh Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrioQueueProject : NSObject
+
+@end
---
+++ PrioQueueProject/PrioQueueProject.m
@@ -0,0 +1,13 @@
+//
+//  PrioQueueProject.m
+//  PrioQueueProject
+//
+//  Created by Josh Smith on 1/20/14.
+//  Copyright (c) 2014 Josh Smith. All rights reserved.
+//
+
+#import "PrioQueueProject.h"
+
+@implementation PrioQueueProject
+
+@end
